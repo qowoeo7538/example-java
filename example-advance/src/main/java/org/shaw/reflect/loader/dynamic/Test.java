e12096e57f36a19c51eea73941befa38e6f07601
zhouxu <01153044@yto.net.cn>
2017-11-07 17:53:43 +0800

    提交

--- example-advance/src/main/java/org/shaw/reflect/loader/dynamic/Test.java
+++ example-advance/src/main/java/org/shaw/reflect/loader/dynamic/Test.java
@@ -4,8 +4,9 @@ package org.shaw.reflect.loader.dynamic;
  * Created by joy on 17-2-6.
  */
 public class Test {
-    public static void main(String[] args)throws Exception {
-        OfficeBetter officeBetter = new OfficeBetter(); //new是对象的静态加载，编译时就会加载;
-        officeBetter.load("com.myweb.reflect.dynamicLoad.Word");
+    public static void main(String[] args) throws Exception {
+        //new是对象的静态加载，编译时就会加载;
+        OfficeBetter officeBetter = new OfficeBetter();
+        OfficeBetter.load("com.myweb.reflect.dynamicLoad.Word");
     }
 }
