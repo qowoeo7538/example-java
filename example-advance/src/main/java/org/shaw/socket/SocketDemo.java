aa0f96d5538fb5c694f22320cc7fa641eda74df1
zhouxu <01153044@yto.net.cn>
2017-11-28 16:53:46 +0800

    提交

--- example-advance/src/main/java/org/shaw/socket/SocketDemo.java
+++
@@ -1,8 +0,0 @@
-package org.shaw.socket;
-
-/**
- * @create: 2017-11-28
- * @description:
- */
-public class SocketDemo {
-}
---
+++ example-advance/src/main/java/org/shaw/socket/rmi/SocketDemo.java
@@ -0,0 +1,11 @@
+package org.shaw.socket.rmi;
+
+/**
+ * @create: 2017-11-28
+ * @description:
+ */
+public class SocketDemo {
+    public static void main(String[] args) {
+
+    }
+}
