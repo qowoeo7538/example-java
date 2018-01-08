f6d771c9952bb8815231e484622bc6fbffaaf584
zhouxu <01153044@yto.net.cn>
2018-01-08 14:56:06 +0800

    Commit

---
+++ example-advance/src/main/java/org/shaw/reflect/clazz/ReflectionNameDemo.java
@@ -0,0 +1,16 @@
+package org.shaw.reflect.clazz;
+
+/**
+ * @create: 2018-01-08
+ * @description:
+ */
+public class ReflectionNameDemo {
+    public static void main(String[] args) {
+        System.out.println(String[].class.getName());
+        System.out.println(Short.class.getName());
+        System.out.println(short[].class.getName());
+        System.out.println(short.class.getName());
+        System.out.println(Void.class.getName());
+        System.out.println(void.class.getName());
+    }
+}
