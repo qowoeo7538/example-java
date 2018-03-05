19396701e8773aebe5dbbd6a0b9cdc8324410614
zhouxu <01153044@yto.net.cn>
2018-03-05 09:13:50 +0800

    Commit

--- example-reflection/src/main/java/org/shaw/base/info/clazz/ClassReflectionDemo.java
+++ example-reflection/src/main/java/org/shaw/base/info/clazz/ClassReflectionDemo.java
@@ -8,36 +8,52 @@ import org.shaw.base.info.clazz.impl.User;
  */
 public class ClassReflectionDemo {
     public static void main(String[] args) throws Exception {
-        /**
-         * 任何一个类都是Class的实例对象
-         * 反射是将字节文件加载到内存里面
-         */
-        // 任何一个类都有一个隐含的静态成员变量 Class
+        // 获取 class 对象
+        getObjectClass();
+
+        // 通过 class 创建对象
+        // createObject();
+    }
+
+    /**
+     * 获取 class 对象
+     * 1)反射是将字节文件加载到内存里面
+     *
+     * @throws ClassNotFoundException
+     */
+    private static void getObjectClass() throws ClassNotFoundException {
+        // 通过隐含的静态成员变量 Class 获取
         Class userClass1 = User.class;
 
-        // 该类的对象通过 getClass() 获取 Class.
+        // 通过 getClass() 获取 Class.
         User user = new User();
         Class userClass2 = user.getClass();
 
-        // 通过类全名获取 Class 对象
-        Class userClass3 = Class.forName("org.shaw.base.info.clazz.impl.User");
-
         /**
-         * foo1, foo2, foo3 表示了Foo类的类类型(class type)
-         * 万事万物皆对象，类也是对象，是Class类的实例对象,这个对象我们称为该类的类类型
+         * name:       类全名
+         * initialize: 是否进行初始化
+         * loader:     类加载器
          */
+        Class userClass3 = Class.forName("org.shaw.base.info.clazz.impl.User", true, ClassReflectionDemo.class.getClassLoader());
+
+        // userClass1, userClass2, userClass3 表示了 User 类的类类型(class type)
         System.out.println(userClass1 == userClass2);
         System.out.println(userClass1 == userClass3);
+        System.out.println(userClass1.isAssignableFrom(userClass2));
+        System.out.println(userClass1.isAssignableFrom(userClass3));
+    }
 
-        /**
-         * 通过类的类类型创建该类的对象实例
-         */
+    /**
+     * 通过 class 创建对象
+     */
+    private static void createObject() throws Exception {
+        Class userClass = User.class;
         // 调用该类的无参构造方法创建实例
-        User user4 = (User) userClass1.newInstance();
-        user4.doSomething();
+        User user = (User) userClass.newInstance();
+        user.doSomething();
         // 通过有参构造方法创建实例
-        User user5 = (User) userClass1.getConstructor(String.class, String.class)
+        User user1 = (User) userClass.getConstructor(String.class, String.class)
                 .newInstance("张三", "123123");
-        user5.doSomething("user5");
+        user1.doSomething("user1");
     }
 }
