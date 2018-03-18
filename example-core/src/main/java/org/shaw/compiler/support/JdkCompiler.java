5392e28b1710cef218f8c9a3384172f1cc01322c
zhouxu <01153044@yto.net.cn>
2018-03-18 14:26:16 +0800

    Commit

--- example-core/src/main/java/org/shaw/compiler/support/JdkCompiler.java
+++ example-core/src/main/java/org/shaw/compiler/support/JdkCompiler.java
@@ -3,7 +3,12 @@ package org.shaw.compiler.support;
 import org.shaw.util.ClassUtils;
 
 import javax.tools.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.net.URLClassLoader;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +16,12 @@ import java.util.List;
  *
  */
 public class JdkCompiler extends AbstractCompiler {
+
+    /** 默认类加载器类名 */
+    private final static String APP_CLASSLOADER = "sun.misc.Launcher$AppClassLoader";
+
+    /** 类加载器 */
+    private final ClassLoaderImpl classLoader;
 
     /** java编译器 */
     private final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
@@ -35,16 +46,44 @@ public class JdkCompiler extends AbstractCompiler {
          */
         StandardJavaFileManager manager = compiler.getStandardFileManager(diagnosticCollector, null, null);
         final ClassLoader loader = ClassUtils.getDefaultClassLoader();
+        /**
+         * 1) 是 URLClassLoader 加载器的实例(通过指向目标文件加载类)
+         * 2) 不是默认的类加载器
+         */
         if (loader instanceof URLClassLoader
-                // 判断是否默认的类加载器
-                && (!loader.getClass().getName().equals("sun.misc.Launcher$AppClassLoader"))) {
-
+                && (!APP_CLASSLOADER.equals(loader.getClass().getName()))) {
+            try {
+                URLClassLoader urlClassLoader = (URLClassLoader) loader;
+                List<File> files = new ArrayList<>();
+                // 获取加载资源的路径
+                for (URL url : urlClassLoader.getURLs()) {
+                    files.add(new File(url.getFile()));
+                }
+                // 将加载的文件与文件管理器关联
+                manager.setLocation(StandardLocation.CLASS_PATH, files);
+            } catch (IOException e) {
+                throw new IllegalStateException(e.getMessage(), e);
+            }
         }
+        classLoader = AccessController.doPrivileged((PrivilegedAction<ClassLoaderImpl>) () ->
+                new ClassLoaderImpl(loader)
+        );
     }
 
     @Override
     protected Class<?> doCompile(String name, String source) throws Throwable {
 
         return null;
+    }
+
+    /**
+     * 类加载器实现
+     */
+    private final class ClassLoaderImpl extends ClassLoader {
+
+        ClassLoaderImpl(final ClassLoader parentClassLoader) {
+            super(parentClassLoader);
+        }
+
     }
 }
