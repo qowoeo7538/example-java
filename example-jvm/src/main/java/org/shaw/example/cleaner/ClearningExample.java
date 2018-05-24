bda4281df432173192d0c142601721e3ce8fea1d
zhouxu <01153044@yto.net.cn>
2018-05-24 09:45:31 +0800

    Commit

---
+++ example-jvm/src/main/java/org/shaw/example/cleaner/ClearningExample.java
@@ -0,0 +1,12 @@
+package org.shaw.example.cleaner;
+
+/**
+ * @create: 2018-05-21
+ * @description:
+ */
+public class ClearningExample implements AutoCloseable {
+    @Override
+    public void close() throws Exception {
+
+    }
+}
