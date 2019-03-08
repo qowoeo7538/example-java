87e5031ff22c82e95beaa7d4965efc9503ec5545
shaw <635776247@qq.com>
2019-03-08 19:19:33 +0800

    Commit

--- example-base/src/main/java/org/shaw/demo/struct/ArrayListDemo.java
+++ example-base/src/main/java/org/shaw/demo/struct/ArrayListDemo.java
@@ -11,7 +11,12 @@ public class ArrayListDemo {
 
     /**
      * ArrayList是一个线程不安全的类，使用迭代器的时候，同时又对集合进行了修改，
-     * 就会抛出 {@code ConcurrentModificationException} 异常
+     * 就会抛出 {@code ConcurrentModificationException} 异常.
+     * <p>
+     * {@link ArrayList.Itr#expectedModCount} 初始化等于 {@link java.util.AbstractList#modCount},
+     * 如果 {@code List} 发生结构修改的时候，{@link java.util.AbstractList#modCount} 会进行+1操作，
+     * {@link Iterator#hasNext()} 会进行检查 {@link ArrayList.Itr#expectedModCount} 和
+     * {@link java.util.AbstractList#modCount} 是否相等，否则抛出 {@code ConcurrentModificationException} 异常.
      */
     @Test(expected = ConcurrentModificationException.class)
     public void iteratorAndRemove() {
