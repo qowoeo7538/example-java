2ef08019682b115dacf21c73346d259859909345
shaw <635776247@qq.com>
2019-04-02 08:13:48 +0800

    Commit

--- example-advance/src/main/java/org/shaw/run/stack/StackDemo.java
+++ example-advance/src/main/java/org/shaw/run/stack/StackDemo.java
@@ -1,5 +1,6 @@
 package org.shaw.run.stack;
 
+import org.junit.Test;
 import org.shaw.run.stack.impl.AStackContent;
 import org.shaw.run.stack.impl.BStackContent;
 
@@ -7,10 +8,15 @@ import org.shaw.run.stack.impl.BStackContent;
  * 运行Stack信息
  */
 public class StackDemo {
-    public static void main(String[] args) {
+
+    @Test
+    public void stackInfo() {
         AStackContent stackContent = new AStackContent();
         stackContent.AATest();
+    }
 
+    @Test
+    public void stackInfo1() {
         BStackContent stackContent1 = new BStackContent();
         stackContent1.BATest();
     }
