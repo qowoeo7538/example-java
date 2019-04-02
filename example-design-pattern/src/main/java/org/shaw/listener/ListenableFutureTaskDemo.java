a21c102c381fed0fff93779c357c09a8ffb90be3
shaw <635776247@qq.com>
2019-04-02 20:56:28 +0800

    Commit

--- example-design-pattern/src/main/java/org/shaw/listener/ListenableFutureTaskDemo.java
+++ example-design-pattern/src/main/java/org/shaw/listener/ListenableFutureTaskDemo.java
@@ -1,21 +1,39 @@
 package org.shaw.listener;
 
+import org.junit.Test;
+import org.shaw.core.Constants;
+import org.shaw.task.ThreadPoolTaskExecutor;
 import org.shaw.util.DataProducerHelper;
 import org.springframework.core.task.support.TaskExecutorAdapter;
 import org.springframework.util.concurrent.ListenableFuture;
 
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @create: 2017-11-24
  * @description:
  */
 public class ListenableFutureTaskDemo {
-    public static void main(String[] args) {
-        ExecutorService executor = Executors.newCachedThreadPool();
-        TaskExecutorAdapter taskExecutorAdapter = new TaskExecutorAdapter(executor);
+
+    private static final int DEFAULT_CORE_POOL_SIZE = Constants.CORE_SIZE + 1;
+    private static final int DEFAULT_MAX_POOL_SIZE = 2 * Constants.CORE_SIZE + 1;
+    private static final int DEFAULT_KEEP_ALIVE_SECONDS = 60;
+    private static final int DEFAULT_QUEUE_CAPACITY = Integer.MAX_VALUE - DEFAULT_MAX_POOL_SIZE;
+
+    private static final ExecutorService threadPoolExecutor =
+            new ThreadPoolTaskExecutor(DEFAULT_CORE_POOL_SIZE,
+                    DEFAULT_MAX_POOL_SIZE,
+                    DEFAULT_KEEP_ALIVE_SECONDS,
+                    DEFAULT_QUEUE_CAPACITY);
+
+    @Test
+    public void listenableFutureTask() {
+        final TaskExecutorAdapter taskExecutorAdapter = new TaskExecutorAdapter(threadPoolExecutor);
         try {
-            ListenableFuture listenableFuture = taskExecutorAdapter.submitListenable(() -> {
+            final ListenableFuture listenableFuture = taskExecutorAdapter.submitListenable(() -> {
                 System.out.println("任务开始");
                 try {
                     TimeUnit.SECONDS.sleep(DataProducerHelper.nextInt(10));
@@ -24,11 +42,13 @@ public class ListenableFutureTaskDemo {
                 }
                 System.out.println("任务结束");
             });
-            CompletableFuture completableFuture = listenableFuture.completable();
+            final CompletableFuture completableFuture = listenableFuture.completable();
             completableFuture.get();
-        } catch (ExecutionException | InterruptedException ex) {
+        } catch (final ExecutionException | InterruptedException ex) {
             Thread.currentThread().interrupt();
+        } finally {
+            threadPoolExecutor.shutdown();
         }
-
     }
+
 }
