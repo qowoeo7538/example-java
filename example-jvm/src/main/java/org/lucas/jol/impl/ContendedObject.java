768fab0241336114860a44718616113a3d0daa46
shaw.zhou <shaw.zhou@winway666.com>
2019-05-24 18:47:44 +0800

    Commit

--- example-jvm/src/main/java/org/lucas/jol/impl/ContendedObject.java
+++ example-jvm/src/main/java/org/lucas/jol/impl/ContendedObject.java
@@ -3,7 +3,7 @@ package org.lucas.jol.impl;
 import jdk.internal.vm.annotation.Contended;
 
 
-public class ContendedObjcet {
+public class ContendedObject {
 
     @Contended
     protected volatile long value;
