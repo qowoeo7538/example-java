ad2fd5019fff33add6befe226e90ff0f94b5e119
zhouxu <zhouxu@wljs.so>
2019-07-12 16:43:45 +0800

    Commint

--- example-core/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module example.core {
-    // 依赖模块
-    requires component.common;
-    requires component.thread;
-
-    // 对外提供模块
-    exports org.lucas.example.core.task;
-    exports org.lucas.example.core.util;
-}
--- example-thread/src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-module example.thread {
-    // 依赖模块
-    requires example.core;
-    requires org.junit.jupiter.api;
-}
