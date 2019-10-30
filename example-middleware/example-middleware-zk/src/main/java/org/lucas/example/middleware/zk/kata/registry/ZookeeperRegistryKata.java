d44b9f5afa2b934220643122b3cca484efd189ca
zhouxu <zhouxu@wljs.so>
2019-10-30 12:06:20 +0800

    Commit

--- example-middleware/example-middleware-zk/src/main/java/org/lucas/example/middleware/zk/kata/registry/ZookeeperRegistryKata.java
+++ example-middleware/example-middleware-zk/src/main/java/org/lucas/example/middleware/zk/kata/registry/ZookeeperRegistryKata.java
@@ -1,8 +1,11 @@
 package org.lucas.example.middleware.zk.kata.registry;
 
+import org.junit.jupiter.api.Test;
+
 public class ZookeeperRegistryKata {
 
     @Test
     public void testAnyHost() {
+
     }
 }
