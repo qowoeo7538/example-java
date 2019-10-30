7637e59ebdd4b158748c746d0af3a243cf3da892
zhouxu <zhouxu@wljs.so>
2019-10-30 12:02:31 +0800

    Commit

--- example-middleware/example-middleware-zk/src/main/java/org/lucas/example/middleware/zk/kata/registry/ZookeeperRegistryKata.java
+++ example-middleware/example-middleware-zk/src/main/java/org/lucas/example/middleware/zk/kata/registry/ZookeeperRegistryKata.java
@@ -4,9 +4,5 @@ public class ZookeeperRegistryKata {
 
     @Test
     public void testAnyHost() {
-        Assertions.assertThrows(IllegalStateException.class, () -> {
-            URL errorUrl = URL.valueOf("multicast://0.0.0.0/");
-            new ZookeeperRegistryFactory().createRegistry(errorUrl);
-        });
     }
 }
