870beff0469c629604d544071abeadf955856e6d
zhouxu <zhouxu@wljs.so>
2019-11-10 10:52:30 +0800

    Commit

---
+++ example-framework/example-framework-eclipse-collections/src/main/java/org/lucas/example/framework/eclipse/collections/demo/LongListDemo.java
@@ -0,0 +1,20 @@
+package org.lucas.example.framework.eclipse.collections.demo;
+
+import org.eclipse.collections.api.list.primitive.LongList;
+import org.eclipse.collections.impl.factory.primitive.LongLists;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.LongStream;
+
+public class LongListDemo {
+
+    @Test
+    public void process() {
+        Long startTime = System.currentTimeMillis();
+        LongList list = LongLists.mutable.ofAll(LongStream.range(0, 1000_0000));
+        list.each(i -> i = +1);
+        list.select(i -> i % 3 == 0);
+        System.out.println(System.currentTimeMillis() - startTime);
+    }
+
+}
