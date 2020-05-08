9ca6f9a1de8f90f2831e31f893d2039bd8f9e2ad
zhouxu <zhouxu@wljs.com>
2020-05-08 14:47:32 +0800

    Commit

---
+++ example-foundation/example-foundation-thread/src/main/java/org/lucas/example/foundation/thread/demo/future/ExecutorCompletionServiceDemo.java
@@ -0,0 +1,32 @@
+package org.lucas.example.foundation.thread.demo.future;
+
+import org.junit.jupiter.api.Test;
+import org.lucas.component.thread.task.ThreadPoolTaskExecutor;
+import org.lucas.example.foundation.core.util.DataProducerHelper;
+
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.Future;
+
+public class ExecutorCompletionServiceDemo {
+
+    @Test
+    public void testExecutorCompletionService() throws InterruptedException, ExecutionException {
+        CompletionService<String> service = new ExecutorCompletionService<>(new ThreadPoolTaskExecutor());
+        for (int i = 0; i < 4; i++) {
+            service.submit(() -> {
+                Thread.sleep(DataProducerHelper.nextInt(100));
+                System.out.println(Thread.currentThread().getName() + "|完成任务");
+                return "data" + DataProducerHelper.nextInt(10);
+            });
+        }
+        for (int j = 0; j < 4; j++) {
+            // 这一行没有完成的任务就阻塞
+            Future<String> take = service.take();
+            // 这一行在这里不会阻塞，引入放入队列中的都是已经完成的任务
+            String result = take.get();
+            System.out.println("获取到结果：" + result);
+        }
+    }
+}
---
+++ example-framework/example-framework-spring/src/main/java/org/lucas/example/framework/spring/thread/impl/AsyncExecutor.java
@@ -0,0 +1,47 @@
+package org.lucas.example.framework.spring.thread.impl;
+
+import org.springframework.core.task.TaskExecutor;
+
+public class AsyncExecutor {
+
+    /**
+     * 线程池执行器
+     */
+    private TaskExecutor taskExecutor;
+
+
+    private class MessagePrinterTask implements Runnable {
+
+        private String message;
+
+        public MessagePrinterTask(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + " " + message);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public TaskExecutor getTaskExecutor() {
+        return taskExecutor;
+    }
+
+    public void setTaskExecutor(TaskExecutor taskExecutor) {
+        this.taskExecutor = taskExecutor;
+    }
+
+    public void printMessages() {
+        for (int i = 0; i < 6; i++) {
+            taskExecutor.execute(new MessagePrinterTask("Message" + i));
+        }
+    }
+
+
+}
