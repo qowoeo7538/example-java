b9d35464109a110d3f986f414b40999ee75e72e2
zhouxu <zhouxu@wljs.com>
2020-07-03 15:53:29 +0800

    commit

---
+++ example-framework/example-framework-rxjava/src/main/java/org/lucas/example/framework/rxjava/common/entity/Person.java
@@ -0,0 +1,46 @@
+package org.lucas.example.framework.rxjava.common.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static List<Person> makeList() {
+        List<Person> personList = new ArrayList<Person>();
+        Person p1 = new Person();
+        p1.setAge(10);
+        p1.setName("zlx");
+        personList.add(p1);
+
+        p1 = new Person();
+        p1.setAge(12);
+        p1.setName("jiaduo");
+        personList.add(p1);
+
+        p1 = new Person();
+        p1.setAge(5);
+        p1.setName("ruoran");
+        personList.add(p1);
+        return personList;
+    }
+
+}
