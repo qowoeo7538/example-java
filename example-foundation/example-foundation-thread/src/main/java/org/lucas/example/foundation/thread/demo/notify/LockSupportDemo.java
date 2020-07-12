b5194efa19fead4bd75eea0b8ff52df1c179f7e6
shaw <635776247@qq.com>
2020-07-13 01:36:45 +0800

    Commit

--- example-foundation/example-foundation-thread/src/main/java/org/lucas/example/foundation/thread/demo/notify/LockSupportDemo.java
+++ example-foundation/example-foundation-thread/src/main/java/org/lucas/example/foundation/thread/demo/notify/LockSupportDemo.java
@@ -5,21 +5,14 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.locks.LockSupport;
 
 /**
- * static Object getBlocker(Thread t)：返回提供给最近一次尚未解除阻塞的 park 方法调用的 blocker 对象，如果该调用不受阻塞，则返回 null。
- * <p>
- * static void park()：为了线程调度，禁用当前线程，除非许可可用。
- * <p>
- * static void park(Object blocker)：为了线程调度，在许可可用之前禁用当前线程。
- * <p>
- * static void parkNanos(long nanos)：为了线程调度禁用当前线程，最多等待指定的等待时间，除非许可可用。
- * <p>
- * static void parkNanos(Object blocker, long nanos)：为了线程调度，在许可可用前禁用当前线程，并最多等待指定的等待时间。
- * <p>
- * static void parkUntil(long deadline)：为了线程调度，在指定的时限前禁用当前线程，除非许可可用。
- * <p>
- * static void parkUntil(Object blocker, long deadline)：为了线程调度，在指定的时限前禁用当前线程，除非许可可用。
- * <p>
- * static void unpark(Thread thread)：如果给定线程的许可尚不可用，则使其可用。
+ * Object getBlocker(Thread t)：                  返回提供给最近一次尚未解除阻塞的 park 方法调用的 blocker 对象，如果该调用不受阻塞，则返回 null。
+ * void park()：                                  为了线程调度，禁用当前线程，除非许可可用。
+ * void park(Object blocker)：                    为了线程调度，在许可可用之前禁用当前线程。
+ * void parkNanos(long nanos)：                   为了线程调度禁用当前线程，最多等待指定的等待时间，除非许可可用。
+ * void parkNanos(Object blocker, long nanos)：   为了线程调度，在许可可用前禁用当前线程，并最多等待指定的等待时间。
+ * void parkUntil(long deadline)：                为了线程调度，在指定的时限前禁用当前线程，除非许可可用。
+ * void parkUntil(Object blocker, long deadline)：为了线程调度，在指定的时限前禁用当前线程，除非许可可用。
+ * void unpark(Thread thread)：                   如果给定线程的许可尚不可用，则使其可用。
  */
 public class LockSupportDemo {
 
