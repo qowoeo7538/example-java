4953b22248c5bdee90e4247339a35a0b2cbaf706
zhouxu <zhouxu@wljs.com>
2020-07-25 14:01:24 +0800

    commit

--- example-framework/example-framework-reactive/example-framework-reactive-reactor/src/main/java/org/lucas/example/framework/reactor/demo/BatchProcessDemo.java
+++ example-framework/example-framework-reactive/example-framework-reactive-reactor/src/main/java/org/lucas/example/framework/reactor/demo/BatchProcessDemo.java
@@ -80,7 +80,7 @@ public class BatchProcessDemo {
 
     @Test
     public void demoReduce(){
-        Flux.range(1, 100).reduce((x, y) -> x + y).subscribe(System.out::println);
+        Flux.range(1, 100).reduce(Integer::sum).subscribe(System.out::println);
         Flux.range(1, 100).reduceWith(() -> 100, (x, y) -> x + y).subscribe(System.out::println);
     }
 
