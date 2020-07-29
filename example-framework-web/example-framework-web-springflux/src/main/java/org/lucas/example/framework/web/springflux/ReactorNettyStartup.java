428904091d569c3d5213cd1efb0d1f6ba32b76d7
zhouxu <zhouxu@wljs.com>
2020-07-29 19:53:14 +0800

    commit

--- example-framework-web/example-framework-web-springflux/src/main/java/org/lucas/example/framework/web/springflux/ReactorNettyStartup.java
+++ example-framework-web/example-framework-web-springflux/src/main/java/org/lucas/example/framework/web/springflux/ReactorNettyStartup.java
@@ -18,9 +18,13 @@ public class ReactorNettyStartup {
                 .port(8080)
                 // 4 设置路由规则
                 .route(routes -> routes
+                        // 为访问路径"/hello"提供 GET 请求并返回 “Hello World!”；
                         .get("/hello", (request, response) -> response.sendString(Mono.just("Hello World!")))
+                        // 为访问路径"/echo"提供 POST 请求，并将收到的请求正文作为响应返回；
                         .post("/echo", (request, response) -> response.send(request.receive().retain()))
+                        // 为访问路径"/path/{param}"提供 GET 请求并返回path参数的值；
                         .get("/path/{param}", (request, response) -> response.sendString(Mono.just(request.param("param"))))
+                        // 将 websocket 提供给"/ws"并将接收的传入数据作为传出数据返回。
                         .ws("/ws", (wsInbound, wsOutbound) -> wsOutbound.send(wsInbound.receive().retain())))
                 .bindNow();
         // 5 阻塞方式启动服务器，同步等待服务停止
