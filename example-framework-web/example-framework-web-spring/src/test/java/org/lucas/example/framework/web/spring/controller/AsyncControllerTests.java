989f353aae69cc449abe8a85c6446c05a9d773c0
zhouxu <zhouxu@wljs.com>
2020-08-17 17:35:26 +0800

    Commit

--- example-framework-web/example-framework-web-spring/src/test/java/org/lucas/example/framework/web/spring/controller/AsyncControllerTests.java
+++ example-framework-web/example-framework-web-spring/src/test/java/org/lucas/example/framework/web/spring/controller/AsyncControllerTests.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
