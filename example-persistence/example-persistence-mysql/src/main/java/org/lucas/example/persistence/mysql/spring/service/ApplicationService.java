4c2a624179eba9864cf76effcc7e4dedc0f26ad7
zhouxu <zhouxu@wljs.com>
2020-09-03 17:19:34 +0800

    commit

--- example-persistence/example-persistence-mysql/src/main/java/org/lucas/example/persistence/mysql/spring/service/ApplicationService.java
+++ example-persistence/example-persistence-mysql/src/main/java/org/lucas/example/persistence/mysql/spring/service/ApplicationService.java
@@ -1,12 +1,17 @@
 package org.lucas.example.persistence.mysql.spring.service;
 
+import org.eclipse.collections.api.factory.Maps;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.lucas.example.persistence.mysql.spring.ApplicationConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.ResultSet;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ApplicationConfig.class)
@@ -16,8 +21,14 @@ public class ApplicationService {
     private JdbcTemplate jdbc;
 
     @Test
-    public void testFindOne(){
-        // jdbc.queryForObject("SELECT * FROM  ")
+    public void testFindOne() {
+        RowMapper<Map<String, String>> rowMapper = (ResultSet rs, int rowNum) -> {
+            Map<String, String> map = Maps.mutable.with();
+            map.put("title", rs.getString("title"));
+            return map;
+        };
+        Map<String, String> map = jdbc.queryForObject("SELECT * FROM t_application WHERE id = ?", rowMapper, 1);
+        System.out.println(map);
     }
 
 }
