5222fefc78a7712c1b4bf617b3bd8214238f40a3
zhouxu <635776247@qq.com>
2021-03-23 20:59:35 +0800

    提交。

--- example-framework/example-framework-disruptor/src/main/java/org/lucas/example/framework/disruptor/demo/LongEventDemo.java
+++ example-framework/example-framework-disruptor/src/main/java/org/lucas/example/framework/disruptor/demo/LongEventDemo.java
@@ -1,6 +1,6 @@
 package org.lucas.example.framework.disruptor.demo;
 
-import com.lmax.disruptor.BlockingWaitStrategy;
+import com.lmax.disruptor.LiteTimeoutBlockingWaitStrategy;
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
 import com.lmax.disruptor.dsl.ProducerType;
@@ -11,6 +11,8 @@ import org.lucas.example.framework.disruptor.demo.consumer.ReplicationConsumer;
 import org.lucas.example.framework.disruptor.demo.event.LongEvent;
 import org.lucas.example.framework.disruptor.demo.event.LongEventFactory;
 import org.lucas.example.framework.disruptor.demo.producer.LongEventProducer;
+
+import java.util.concurrent.TimeUnit;
 
 public class LongEventDemo {
 
@@ -24,8 +26,8 @@ public class LongEventDemo {
         // 3 构造Disruptor,其构造函数会根据 bufferSize 大小调用 LongEventFactory 创建对应个数的事件
         // 对象（事件预分配），并且这里使用 DaemonThreadFactory.INSTANCE 作为其背后异步任务调用的线程
         // 池（可以自定义），另外，因为只有一个生产者，所以可以设置生产者模式为 ProducerType.SINGLE 以
-        // 便遵循 Single Writer 原则，减少 CAS 操作，最后设置 RingBuffer 的等待策略为 BlockingWaitStrategy。
-        Disruptor<LongEvent> disruptor = new Disruptor<>(factory, bufferSize, DaemonThreadFactory.INSTANCE, ProducerType.SINGLE, new BlockingWaitStrategy());
+        // 便遵循 Single Writer 原则，减少 CAS 操作，最后设置 RingBuffer 的等待策略为 LiteTimeoutBlockingWaitStrategy、BlockingWaitStrategy。
+        Disruptor<LongEvent> disruptor = new Disruptor<>(factory, bufferSize, DaemonThreadFactory.INSTANCE, ProducerType.SINGLE, new LiteTimeoutBlockingWaitStrategy(1000, TimeUnit.MILLISECONDS));
 
         // 4 注册消费者，同一个元素被 JournalConsumer 和 ReplicationConsumer 消费后，
         // ApplicationConsumer 才可以消费对应的元素。
