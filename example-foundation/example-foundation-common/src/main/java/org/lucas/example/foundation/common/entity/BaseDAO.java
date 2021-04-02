c91e7e8435445c168f97efd75f472c6413f4a8f8
zhouxu <635776247@qq.com>
2021-04-02 18:03:35 +0800

    提交.

---
+++ example-foundation/example-foundation-common/src/main/java/org/lucas/example/foundation/common/entity/BaseDAO.java
@@ -0,0 +1,14 @@
+package org.lucas.example.foundation.common.entity;
+
+public class BaseDAO<T> {
+
+    private Class<T> clazz;
+
+    public Class<T> getClazz() {
+        return clazz;
+    }
+
+    public void setClazz(Class<T> clazz) {
+        this.clazz = clazz;
+    }
+}
--- example-foundation/example-foundation-reflection/src/main/java/org/lucas/example/foundation/reflection/demo/info/GetGenericInfoDemo.java
+++ example-foundation/example-foundation-reflection/src/main/java/org/lucas/example/foundation/reflection/demo/info/GetGenericInfoDemo.java
@@ -1,7 +1,34 @@
 package org.lucas.example.foundation.reflection.demo.info;
 
+import org.junit.jupiter.api.Test;
+import org.lucas.example.foundation.common.entity.BaseDAO;
+import org.lucas.example.foundation.common.entity.User;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+/**
+ * 获取泛型
+ */
 public class GetGenericInfoDemo {
 
+    @Test
+    public void getSuperclassGenericType() {
+        // BaseDAO 的匿名子类
+        BaseDAO<User> dao = new BaseDAO<>() {
+        };
+        System.out.println(getGenericClassByIndex(dao.getClass().getGenericSuperclass(), 0));
+    }
 
+    private static Type getGenericClassByIndex(Type genericType, int index) {
+        Type clazz = null;
+        // find parameterized type
+        if (genericType instanceof ParameterizedType) {
+            ParameterizedType t = (ParameterizedType) genericType;
+            Type[] types = t.getActualTypeArguments();
+            clazz = types[index];
+        }
+        return clazz;
+    }
 
 }
