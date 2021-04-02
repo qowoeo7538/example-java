b99fcc62e7fbafd4094fe7ce9359d1faa347b911
zhouxu <635776247@qq.com>
2021-04-02 18:29:59 +0800

    提交.

--- example-foundation/example-foundation-reflection/src/main/java/org/lucas/example/foundation/reflection/demo/info/GetGenericInfoDemo.java
+++ example-foundation/example-foundation-reflection/src/main/java/org/lucas/example/foundation/reflection/demo/info/GetGenericInfoDemo.java
@@ -6,11 +6,24 @@ import org.lucas.example.foundation.common.entity.User;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 
 /**
  * 获取泛型
  */
 public class GetGenericInfoDemo {
+
+    /**
+     * 方法中通过map.getClass()是永远无法获取的
+     * 类，是属性和方法的集合，类上泛型的定义，本质是供属性和方法的定义和使用
+     * map的泛型的真实类型，可通过属性和方法获取
+     */
+    @Test
+    public void getClassGenericType() {
+        BaseDAO<User> dao = new BaseDAO<>();
+        TypeVariable<?>[] genericSuperType = dao.getClass().getTypeParameters();
+        System.out.println(genericSuperType[0]);
+    }
 
     @Test
     public void getSuperclassGenericType() {
