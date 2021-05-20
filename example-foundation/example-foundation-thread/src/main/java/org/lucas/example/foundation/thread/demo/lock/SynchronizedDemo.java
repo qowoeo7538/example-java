d71fe3cdcacb1698960aeaa053f84af062ae5223
zhouxu <635776247@qq.com>
2021-05-20 18:27:11 +0800

    提交.

--- example-foundation/example-foundation-thread/src/main/java/org/lucas/example/foundation/thread/demo/lock/SynchronizedDemo.java
+++ example-foundation/example-foundation-thread/src/main/java/org/lucas/example/foundation/thread/demo/lock/SynchronizedDemo.java
@@ -1,6 +1,10 @@
 package org.lucas.example.foundation.thread.demo.lock;
 
 import org.junit.Test;
+import org.lucas.example.foundation.thread.demo.lock.support.ClassLock;
+import org.lucas.example.foundation.thread.demo.lock.support.ObjectLock;
+
+import static org.lucas.example.foundation.thread.demo.lock.support.ClassLock.add;
 
 /**
  * 对象锁 和 类锁
@@ -9,9 +13,74 @@ import org.junit.Test;
  */
 public class SynchronizedDemo {
 
+    /**
+     * 为什么要用final修饰:
+     * <p>
+     * 内部类对象的生命周期会超过局部变量的生命周期。
+     * 局部变量的生命周期：当该方法被调用时，该方法中的局部变量在栈中被创建，当方法调用结束时，退栈，这些局部变量全部死亡。
+     * 而内部类对象生命周期与其它类一样：自创建一个匿名内部类对象，系统为该对象分配内存，直到没有引用变量指向分配给该对象的内存，它才会死亡(被JVM垃圾回收)。
+     * 所以完全可能出现的一种情况是：成员方法已调用结束，局部变量已死亡，但匿名内部类的对象仍然活着。
+     * <p>
+     * 匿名内部类对象可以访问同一个方法中被定义为final类型的局部变量。
+     * 定义为final后，编译程序的实现方法：对于匿名内部类对象要访问的所有final类型局部变量，都拷贝成为该对象中的一个数据成员。
+     * 这样，即使栈中局部变量已死亡，但被定义为final类型的局部变量的值永远不变，因而匿名内部类对象在局部变量死亡后，照样可以访问final类型的局部变量，因为它自己拷贝了一份，且与原局部变量的值始终一致。
+     */
     @Test
-    public void testObjectLock(){
+    public void testObjectLock() throws Exception {
+        final ObjectLock thread1 = new ObjectLock();
+        final ObjectLock thread2 = new ObjectLock();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // 1 同一个对象、同一把锁
+                // 内部类无法访问非final对象
+                thread1.add();
+            }
+        }, "thread1");
 
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //1、同一个对象、同一把锁
+                //thread1.add();
+                thread2.add();
+            }
+        }, "thread2");
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+    }
+
+    @Test
+    public void testClassLock() throws Exception {
+        final ClassLock thread1 = new ClassLock();
+        final ClassLock thread2 = new ClassLock();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // 1 同一个对象、同一把锁
+                // 内部类无法访问非final对象
+                add();
+            }
+        }, "thread1");
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //1、同一个对象、同一把锁
+                //thread1.add();
+                add();
+            }
+        }, "thread2");
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
     }
 
 }
--- example-foundation/example-foundation-thread/src/main/java/org/lucas/example/foundation/thread/demo/lock/support/ClassLock.java
+++ example-foundation/example-foundation-thread/src/main/java/org/lucas/example/foundation/thread/demo/lock/support/ClassLock.java
@@ -1,16 +1,13 @@
 package org.lucas.example.foundation.thread.demo.lock.support;
 
-/**
- * 对象锁
- */
-public class ObjectLock extends Thread {
+public class ClassLock {
 
-    private int count = 0;
+    private static int count = 0;
 
     /**
-     *  synchronized 是获得对象锁,如果作用在static类型上,则升级为类锁
+     * synchronized 是获得对象锁,如果作用在static类型上,则升级为类锁
      */
-    public synchronized void add() {
+    public synchronized static void add() {
         count++;
         try {
             Thread.sleep(1000);
@@ -20,5 +17,4 @@ public class ObjectLock extends Thread {
         }
         System.out.println(Thread.currentThread().getName() + ">count=" + count);
     }
-
 }
