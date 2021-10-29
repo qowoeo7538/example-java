4a9d9063596022844e069bd8611a14f0ae3a6c46
zhouxu <zhou.xu@xyb2b.com>
2021-10-29 17:41:19 +0800

    提交.

--- example-algorithm/src/main/java/org/lucas/example/algorithm/timingwheel/TimingWheelDemo.java
+++ example-algorithm/src/main/java/org/lucas/example/algorithm/timingwheel/TimingWheelDemo.java
@@ -13,35 +13,35 @@ class TimingWheelDemo {
     static int runCount = 0;
 
     @Test
-    void demoTimer(){
+    void demoTimer() {
         CountDownLatch countDownLatch = new CountDownLatch(1000);
         Timer timer = new Timer();
-        for(int i=1;i<=1000;i++){
-            TimerTask timerTask = new TimerTask(i,()->{
+        for (int i = 1; i <= 1000; i++) {
+            TimerTask timerTask = new TimerTask(i, () -> {
                 countDownLatch.countDown();
                 int index = addRun();
-                System.out.println(index+"----------执行");
+                System.out.println(index + "----------执行");
             });
             timer.addTask(timerTask);
-            System.out.println(i+"++++++++++加入");
+            System.out.println(i + "++++++++++加入");
             inCount++;
         }
-        TimerTask timerTask = new TimerTask(5000,()->{
+        TimerTask timerTask = new TimerTask(5000, () -> {
             countDownLatch.countDown();
             int index = addRun();
-            System.out.println(index+"----------执行");
+            System.out.println(index + "----------执行");
         });
         timer.addTask(timerTask);
         try {
             countDownLatch.await();
             System.out.println("inCount" + inCount);
             System.out.println("runCount" + runCount);
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public synchronized static int addRun(){
+    public synchronized static int addRun() {
         runCount++;
         return runCount;
     }
